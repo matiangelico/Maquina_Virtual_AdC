5c833ab0357fef5b37a59c29e091c31d33772861
Santi <santilapiana02@gmail.com>
2022-04-17 23:54:50 -0300

    Terminado ESPERO

---
+++ Codigo Fuente Ejecutor/src/main/Main.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import paquete.ALU;
+import paquete.MaquinaVirtual;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		int i, inst, CS = 0;
+		String binFilename = null;
+		
+		MaquinaVirtual maquinavirtual = new MaquinaVirtual();
+		for (i = 0; i < args.length; i++) {
+            if (args[i].endsWith("-b"))
+                maquinavirtual.setParametrob(true);
+            else if (args[i].endsWith("-c")) {
+                maquinavirtual.setParametroc(true);
+            }
+            else if (args[i].endsWith("-d"))
+                maquinavirtual.setParametrod(true);
+            else if (args[i].endsWith(".mv1"))
+                binFilename = args[i];
+        }
+		
+		if (binFilename != null) {	// Si encuentra el archivo
+			FileInputStream arch = new FileInputStream(binFilename);
+			DataInputStream entrada = new DataInputStream(arch);
+
+			for (i = 0; i < 6; i++) {					// lee el header
+				inst = entrada.readInt();
+				if (i == 1) {		// el 2do bloque tiene el tamaño del codigo 
+					CS = inst;
+				}
+			}
+
+			maquinavirtual.setDS(CS);
+
+			for (i = 0; i < CS; i++) {					// carga instrucciones en la RAM
+				inst = entrada.readInt();
+				maquinavirtual.cargainstruccion(inst, i);
+			}
+			arch.close();
+			entrada.close();
+			
+			if (ALU.getParametroc())
+				new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+
+			do {										// lee y ejecuta codigo desde la RAM
+				inst = maquinavirtual.getInstruccion();
+				maquinavirtual.incrementaIP();
+				maquinavirtual.ejecutaInstruccion(inst);
+				if (maquinavirtual.isP() && !maquinavirtual.isBreakpoint(inst))
+					maquinavirtual.sys();
+			} while ((0 <= maquinavirtual.getIP()) && (maquinavirtual.getIP() < maquinavirtual.getDS()));
+		} else 
+			System.out.println("El archivo .mv1 a leer no se encontró en la lista de parametros");
+	}
+}
---
+++ Codigo Fuente Ejecutor/src/paquete/MaquinaVirtual.java
@@ -0,0 +1,98 @@
+package paquete;
+
+import java.io.IOException;
+
+public class MaquinaVirtual {
+
+	private Memoria memoria;
+	private Registros registros;
+	private ALU alu;
+	
+	private static boolean p = false;
+	public boolean isP() {
+		return p;
+	}
+	
+	public boolean isBreakpoint(int inst) {
+		return inst == 0xF000000F;
+	}
+	
+	public void sys() throws InterruptedException, IOException {
+		this.alu.sys(memoria, registros, 0x1000000F);	// se fuerza el valor de un sys %F y el 1 es para evitar que se limpie la pantalla
+	}
+	
+	public static void setP(boolean p) {
+		MaquinaVirtual.p = p;
+	}
+	
+	public MaquinaVirtual() {
+	
+		this.memoria = new Memoria();
+		this.registros = new Registros();
+		this.alu = new ALU();
+	
+	}
+	
+	public void setParametrob(boolean paramB) {
+        alu.setParametroB(paramB);
+    }
+	
+    public  void setParametroc(boolean paramC) {
+        alu.setParametroC(paramC);
+    }
+    
+    public  void setParametrod(boolean paramD) {
+        alu.setParametroD(paramD);
+    }
+
+	public void cargainstruccion(int instruccion,int i) {
+		this.memoria.cargainstruccion(instruccion,i);
+	}
+	
+	public int getInstruccion() {
+		return this.memoria.getInstruccion(registros.getIP());
+	}
+	
+	public void ejecutaInstruccion(int instruccion) throws InterruptedException, IOException {
+		alu.ejecutaInstruccion(instruccion, this.memoria, this.registros);
+	}
+	
+	public void setDS(int DS) {
+		this.registros.setDS(DS);
+	}
+	
+	public int getDS() {
+		return this.registros.getDS();
+	}
+	
+	public int getIP() {
+		return this.registros.getIP();
+	}
+	
+	
+	public void incrementaIP() {
+		this.registros.incrementaIP();
+	}
+	
+	public int getEAX() {
+		return this.registros.getAX();
+	}
+	
+	public void setIP(int valor) {
+		this.registros.setIP(valor);
+	}
+	
+	public void setregistroo(String nombre,int valor) {
+		registros.setRegistro(nombre, valor);
+	}
+	
+	public int devuelveCelda(int numcelda) {
+		return memoria.getValorRAM(numcelda);
+	}
+	
+	public boolean esNegativoo(int numero) {
+		return this.alu.esNegativo(numero,0xFF);
+	}
+	
+
+}
